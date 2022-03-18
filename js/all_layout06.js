ffefe7506a044bffb6ea63dd37c73695b7a759c4
모시깽이 <100889974+mosiggaengi@users.noreply.github.com>
2022-03-18 17:19:55 +0900

    Add files via upload

--- js/all_layout06.js
+++ js/all_layout06.js
@@ -9,17 +9,74 @@ $(function(){
 
     
     $('.mainSlide').slick({
-        arrows:false,
+        arrows:true,
         autoplay:true,
         autoplaySpeed:3000,
         dots:true,
+        nextArrow: '<div class="rightArrow"></div>',
+        prevArrow: '<div class="leftArrow"></div>'
+    });
+    
+    
+    // 슬라이드에 애니메이션 주기
+    $('.mainSlide figure').eq(1).addClass('oo');
+    $('.mainSlide').on('afterChange',function(e,s,c){
+      console.log(e,s,c);
+      $('.mainSlide figure').eq(c+1).addClass('oo').siblings().removeClass('oo');
+      if (c===1) {
+        $('.leftArrow').addClass('oo')
+      } else {
+        $('.leftArrow').removeClass('oo')
+      }
+      
     });
 
+    $('.eProductSlide').slick({
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:2000,
+    });
+
+
+    
+    
+    $('.eventProduct i:nth-of-type(1)').on('click', function(){
+       $('.eProductSlide').slick('slickPause')
+    });
+  
+    $('.eventProduct i:nth-of-type(2)').on('click', function(){
+       $('.eProductSlide').slick('slickPlay')
+    });
+
+
+    $('.aproductSlide').slick({
+        infinite:true,
+        arrows:false,
+        dots:true,
+        slidesToShow:5,
+        slidesToScroll:1,
+        autoplay:false,
+        autoplaySpeed:1000,
+        centerMode: true,
+        centerPadding: '0',
+
+
+
+        
+    });
+
+    $('.allProduct i:first-child').on('click', function(){
+       $('.aproductSlide').slick('slickPrev')
+    });
+    
+    $('.allProduct i:last-child').on('click', function(){
+       $('.aproductSlide').slick('slickNext')
+    });
+  
     
 
-
-
-
+    ////////////////////////////// 
+    
 
 
 
